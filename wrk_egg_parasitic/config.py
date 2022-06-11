3618b3fd86d8546e6661486ec280af20e7dd17f9
SnkrBhr16 <79738178+SnkrBhr16@users.noreply.github.com>
2022-06-11 21:26:05 +0530

    Update config.py
    updated

--- wrk_egg_parasitic/config.py
+++ wrk_egg_parasitic/config.py
@@ -17,71 +17,13 @@ model = dict(
     test_cfg=dict(score_thr=0.01, nms=dict(type='nms', iou_threshold=0.65)))
 data_root = '/home/dgxuser27/icip/Chula-Egg/'
 dataset_type = 'CocoDataset'
-train_pipeline = [
-    dict(type='Mosaic', img_scale=(640, 640), pad_val=114.0),
-    dict(
-        type='RandomAffine', scaling_ratio_range=(0.1, 2),
-        border=(-320, -320)),
-    dict(
-        type='MixUp',
-        img_scale=(640, 640),
-        ratio_range=(0.8, 1.6),
-        pad_val=114.0),
-    dict(type='YOLOXHSVRandomAug'),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Resize', img_scale=(640, 640), keep_ratio=True),
-    dict(
-        type='Pad',
-        pad_to_square=True,
-        pad_val=dict(img=(114.0, 114.0, 114.0))),
-    dict(type='FilterAnnotations', min_gt_bbox_wh=(1, 1), keep_empty=False),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-]
+train_pipeline = None
 classes = ('Ascaris lumbricoides', 'Capillaria philippinensis',
            'Enterobius vermicularis', 'Fasciolopsis buski', 'Hookworm egg',
            'Hymenolepis diminuta', 'Hymenolepis nana',
            'Opisthorchis viverrine', 'Paragonimus spp', 'Taenia spp. egg',
            'Trichuris trichiura')
-train_dataset = dict(
-    type='MultiImageMixDataset',
-    dataset=dict(
-        type='CocoDataset',
-        ann_file='/home/dgxuser27/icip/Chula-Egg/labeled/train/labels.json',
-        img_prefix='/home/dgxuser27/icip/Chula-Egg/labeled/data/',
-        classes=('Ascaris lumbricoides', 'Capillaria philippinensis',
-                 'Enterobius vermicularis', 'Fasciolopsis buski',
-                 'Hookworm egg', 'Hymenolepis diminuta', 'Hymenolepis nana',
-                 'Opisthorchis viverrine', 'Paragonimus spp',
-                 'Taenia spp. egg', 'Trichuris trichiura'),
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(type='LoadAnnotations', with_bbox=True)
-        ],
-        filter_empty_gt=False),
-    pipeline=[
-        dict(type='Mosaic', img_scale=(640, 640), pad_val=114.0),
-        dict(
-            type='RandomAffine',
-            scaling_ratio_range=(0.1, 2),
-            border=(-320, -320)),
-        dict(
-            type='MixUp',
-            img_scale=(640, 640),
-            ratio_range=(0.8, 1.6),
-            pad_val=114.0),
-        dict(type='YOLOXHSVRandomAug'),
-        dict(type='RandomFlip', flip_ratio=0.5),
-        dict(type='Resize', img_scale=(640, 640), keep_ratio=True),
-        dict(
-            type='Pad',
-            pad_to_square=True,
-            pad_val=dict(img=(114.0, 114.0, 114.0))),
-        dict(
-            type='FilterAnnotations', min_gt_bbox_wh=(1, 1), keep_empty=False),
-        dict(type='DefaultFormatBundle'),
-        dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-    ])
+train_dataset = None
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
@@ -100,80 +42,16 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=12,
+    samples_per_gpu=8,
     workers_per_gpu=6,
     persistent_workers=True,
-    train=dict(
-        type='MultiImageMixDataset',
-        dataset=dict(
-            type='CocoDataset',
-            ann_file='/home/dgxuser27/icip/Chula-Egg/labeled/train/labels.json',
-            img_prefix='/home/dgxuser27/icip/Chula-Egg/labeled/data/',
-            classes=('Ascaris lumbricoides', 'Capillaria philippinensis',
-                     'Enterobius vermicularis', 'Fasciolopsis buski',
-                     'Hookworm egg', 'Hymenolepis diminuta',
-                     'Hymenolepis nana', 'Opisthorchis viverrine',
-                     'Paragonimus spp', 'Taenia spp. egg',
-                     'Trichuris trichiura'),
-            pipeline=[
-                dict(type='LoadImageFromFile'),
-                dict(type='LoadAnnotations', with_bbox=True)
-            ],
-            filter_empty_gt=False),
-        pipeline=[
-            dict(
-                type='RandomAffine',
-                scaling_ratio_range=(0.1, 2),
-                border=(-320, -320)),
-            dict(
-                type='MixUp',
-                img_scale=(640, 640),
-                ratio_range=(0.8, 1.6),
-                pad_val=114.0),
-            dict(type='YOLOXHSVRandomAug'),
-            dict(type='Resize', img_scale=(640, 640), keep_ratio=True),
-            dict(
-                type='Pad',
-                pad_to_square=True,
-                pad_val=dict(img=(114.0, 114.0, 114.0))),
-            dict(
-                type='FilterAnnotations',
-                min_gt_bbox_wh=(1, 1),
-                keep_empty=False),
-            dict(type='DefaultFormatBundle'),
-            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-        ]),
-    val=dict(
-        type='CocoDataset',
-        ann_file='/home/dgxuser27/icip/Chula-Egg/labeled/test/test.json',
-        img_prefix='/home/dgxuser27/icip/Chula-Egg/labeled/data/',
-        classes=('Ascaris lumbricoides', 'Capillaria philippinensis',
-                 'Enterobius vermicularis', 'Fasciolopsis buski',
-                 'Hookworm egg', 'Hymenolepis diminuta', 'Hymenolepis nana',
-                 'Opisthorchis viverrine', 'Paragonimus spp',
-                 'Taenia spp. egg', 'Trichuris trichiura'),
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(
-                type='MultiScaleFlipAug',
-                img_scale=(640, 640),
-                flip=False,
-                transforms=[
-                    dict(type='Resize', keep_ratio=True),
-                    dict(type='RandomFlip'),
-                    dict(
-                        type='Pad',
-                        pad_to_square=True,
-                        pad_val=dict(img=(114.0, 114.0, 114.0))),
-                    dict(type='DefaultFormatBundle'),
-                    dict(type='Collect', keys=['img'])
-                ])
-        ]),
+    train=None,
+    val=None,
     test=dict(
         type='CocoDataset',
         ann_file=
-        '/home/dgxuser27/icip/Chula-Egg/unlabeled/annotations/test.json',
-        img_prefix='/home/dgxuser27/icip/Chula-Egg/unlabeled/data/',
+        './Chula-Egg/unlabeled/annotations/test.json',
+        img_prefix='./Chula-Egg/unlabeled/data/',
         classes=('Ascaris lumbricoides', 'Capillaria philippinensis',
                  'Enterobius vermicularis', 'Fasciolopsis buski',
                  'Hookworm egg', 'Hymenolepis diminuta', 'Hymenolepis nana',
@@ -203,4 +81,4 @@ evaluation = dict(
     save_best='auto', interval=10, dynamic_intervals=[(285, 1)], metric='bbox')
 work_dir = 'wrk_egg_parasatic'
 auto_resume = False
-gpu_ids = range(0, 4)
+gpu_ids = range(0, 1)
